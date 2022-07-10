e7eee870a8b8ffd51cd05bd13c0d8c6213ed9be9
pluggPreagar <pluggPreagar@preisfrieden.de>
2022-07-10 20:33:36 +0200

    sdCardReader

---
+++ sdCardReader/sdCardReader/sdCardReader.ino
@@ -0,0 +1,47 @@
+#include <SPI.h>
+#include <SD.h>
+
+File dataFile;
+
+void setup() 
+{
+    // Open serial communications and wait for port to open:
+    Serial.begin(9600);
+    while (!Serial); // wait for serial port to connect. Needed for native USB port only
+
+    Serial.print("Initializing SD card...");
+    if (!SD.begin(10)) 
+    {
+        Serial.println("initialization failed!");
+        while (1);
+    }
+    Serial.println("initialization done.");
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    
+    dataFile = SD.open("data.txt", FILE_WRITE);
+    
+    // if the file opened okay, write to it:
+    if (dataFile) 
+    {
+        Serial.print("Writing to data.txt...");
+        dataFile.println("This is a data file :)");
+        dataFile.println("testing 1, 2, 3.");
+        for (int i = 0; i < 20; i++) 
+        {
+            dataFile.println(i);
+        }
+        // close the file:
+        dataFile.close();
+        Serial.println("done.");
+    } 
+    else 
+    {
+        // if the file didn't open, print an error:
+        Serial.println("error opening data.txt");
+    }
+}
+void loop() 
+{
+// nothing happens after setup
+}
--- smartMetUino.ino
+++ smartMetUino.ino
@@ -1,14 +1,29 @@
 byte inByte = 0;         // incoming serial byte
 byte match=0;
-byte pattern[] = {0x77 ,0x07 ,0x01 ,0x00 ,0x01 ,0x08 ,0x00 ,0xFF ,0x64 ,0x1C ,0x21 ,0x04 ,0x72 ,0x62 ,0x01 ,0x65 ,0x03 ,0x35 ,0xBF ,0x79 ,0x62 ,0x1E ,0x52 ,0xFF ,0x65};
+byte PLACEHOLDER = 0xFE;
+byte TYPE = 0xFD;
+byte SKIPP_BETWEEN = 0xFC;
+//byte pattern[] = {0x77 ,0x07 ,0x01 ,0x00 ,PLACEHOLDER ,0x08 ,PLACEHOLDER ,0xFF ,0x64 ,0x1C ,0x21 ,0x04 ,0x72 ,0x62 ,0x01 ,0x65 ,0x03 ,0x35 ,0xBF ,0x79 ,0x62 ,0x1E ,0x52 ,0xFF ,0x65};
+byte pattern[] = {0x77 ,0x07 ,0x01 ,0x00 ,PLACEHOLDER ,PLACEHOLDER ,PLACEHOLDER ,0xFF , SKIPP_BETWEEN ,0xFF ,TYPE};
 byte pattern_len = sizeof(pattern)/ sizeof(byte);
 byte digit_len = 4;
 unsigned long digit = 0;
 byte row_len = pattern_len + digit_len;
 
+#if 0
+
 #define LOGw(s) Serial.write(s)
 #define LOG_(s) Serial.print(s)
 #define LOG(s)  Serial.println(s)
+
+#else
+
+#define LOGw(s) inByte
+#define LOG_(s) inByte
+#define LOG(s)  inByte
+
+
+#endif
 
 void setup() {
   Serial.begin(9600);
@@ -35,10 +50,18 @@ void loop() {
         match = 0;
         digit = 0; 
       }
-    } else if (inByte == pattern[match++] )  { // char(FF) != byte(255)  !!!
+    } else if (SKIPP_BETWEEN == pattern[match] && (inByte != pattern[match+1] || 0==++match))  { // tricke negative sideeffect to incr-match and still keep searching condition ...
+    } else if (inByte == pattern[match])  { // char(FF) != byte(255)  !!!
       LOG_("= ");
+      match++;
+    } else if (PLACEHOLDER == pattern[match] )  { 
+      Serial.print(inByte);  Serial.print( 0xFF == pattern[match+1] ? ";" : ".");
+      match++;
+    } else if (TYPE == pattern[match] )  { // value tells about values type 
+      if ( 0x62 == inByte || 0x65 == inByte ) row_len = pattern_len + (0x62 == inByte ? 1 : 4 );
+      match++;
     } else {
-      LOG_("<");LOG_(inByte);LOG_("<->");LOG_(pattern[match-1]);LOG("> ");
+      LOG_("<");LOG_(inByte);LOG_("<->");LOG_(pattern[match]);LOG("> ");
       match = 0;
     } // match
   } // serial.avail
