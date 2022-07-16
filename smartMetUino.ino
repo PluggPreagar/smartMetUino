92382c5bf9aa5ca4122208de91c902b49e1d8d24
pluggPreagar <pluggPreagar@preisfrieden.de>
2022-07-16 14:31:59 +0200

    add timer to clear incomplete messages

--- smartMetUino.ino
+++ smartMetUino.ino
@@ -1,19 +1,3 @@
-byte inByte = 0;         // incoming serial byte
-
-byte PLACEHOLDER = 0xFE;
-byte TYPE = 0xFD;
-byte SKIPP_BETWEEN = 0xFC;
-//byte pattern[] = {0x77 ,0x07 ,0x01 ,0x00 ,PLACEHOLDER ,0x08 ,PLACEHOLDER ,0xFF ,0x64 ,0x1C ,0x21 ,0x04 ,0x72 ,0x62 ,0x01 ,0x65 ,0x03 ,0x35 ,0xBF ,0x79 ,0x62 ,0x1E ,0x52 ,0xFF ,0x65};
-byte pattern[] = {0x77 ,0x07 ,0x01 ,0x00 ,PLACEHOLDER ,0x08 ,PLACEHOLDER ,0xFF , SKIPP_BETWEEN ,0xFF ,TYPE};
-byte pattern_len = sizeof(pattern)/ sizeof(byte);
-
-byte row_len[2] = {};
-byte _match[2] = {};
-byte digit_idx[2] = {};
-#define DIGIT_MAX_VALUE_COUNT 6
-#define DIGIT_VALUE_SIZE DIGIT_MAX_VALUE_COUNT+1
-unsigned long digit[2][ DIGIT_VALUE_SIZE ] = {};
-
 
 #if 0
 
@@ -39,6 +23,35 @@ SoftwareSerial mySerial_0 =  SoftwareSerial(rxPin_0, txPin_0);
 #define txPin_1 8
 SoftwareSerial mySerial_1 =  SoftwareSerial(rxPin_1, txPin_1);
 
+#define ledPin_Board 13
+#define ledPin_0  4
+#define ledPin_1  3
+
+
+byte inByte = 0;         // incoming serial byte
+
+byte PLACEHOLDER   = 0xFE;
+byte SKIPP_BETWEEN = 0xFC;
+byte TYPE          = 0xFD;
+//byte pattern[] = {0x77 ,0x07 ,0x01 ,0x00 ,PLACEHOLDER ,0x08 ,PLACEHOLDER ,0xFF ,0x64 ,0x1C ,0x21 ,0x04 ,0x72 ,0x62 ,0x01 ,0x65 ,0x03 ,0x35 ,0xBF ,0x79 ,0x62 ,0x1E ,0x52 ,0xFF ,0x65};
+byte pattern[] = {0x77 ,0x07 ,0x01 ,0x00 ,PLACEHOLDER ,0x08 ,PLACEHOLDER ,0xFF , SKIPP_BETWEEN ,0xFF ,TYPE};
+byte pattern_len = sizeof(pattern)/ sizeof(byte);
+
+byte row_len[2] = {};
+byte _match[2] = {};
+byte digit_idx[2] = {};
+
+#define DIGIT_MAX_VALUE_COUNT 6
+#define DIGIT_VALUE_SIZE DIGIT_MAX_VALUE_COUNT+1
+unsigned long digit[2][ DIGIT_VALUE_SIZE ] = {};
+byte          ledPin[2] = { ledPin_0, ledPin_1  };
+
+
+// max delay (after last serial*.available) in milli-sec to reset serial-data as "incomplete"
+#define SERIAL_TIMER_DISABLE  1
+#define SERIAL_TIMER_CLEAR    0
+#define SERIAL_TIMER_DELAY  100
+unsigned long timer[2] = {};
 
 
 void setup() {
@@ -58,6 +71,10 @@ void setup() {
   mySerial_1.begin(9600);
 
 
+  // LED
+  pinMode(ledPin_0  , OUTPUT);
+  pinMode(ledPin_1  , OUTPUT);
+
   Serial.print( mySerial_0.isListening() ? "0" : "_" );Serial.print( mySerial_1.isListening() ? "1" : "_" );Serial.println( " is on");
 
 
@@ -76,26 +93,59 @@ void writeSML(){
     }
   }
   Serial.println("");
-  memset(digit, 0, sizeof(digit));
   memset(_match, 0, sizeof(_match));
+  memset(digit, 0, sizeof(digit));
   memset(digit_idx, 0, sizeof(digit_idx));
+}
+
+void resetSML(){
+  memset(_match, 0, sizeof(_match));
+  memset(digit, 0, sizeof(digit));
+  memset(digit_idx, 0, sizeof(digit_idx));
+}
+
+void resetSML(byte srcId){
+  _match[ srcId ] = 0;
+  for(byte i=0; i < DIGIT_MAX_VALUE_COUNT; i++) digit[ srcId ][ i ] = 0;
+  digit_idx[ srcId ] = 0;
+}
+
+
+void checkTimer( byte srcId) {
+  if ( SERIAL_TIMER_DISABLE == timer[ srcId ]) {
+  } else if ( SERIAL_TIMER_CLEAR == timer[ srcId ]) {
+    timer[ srcId ] = millis() + SERIAL_TIMER_DELAY ;
+  } else if ( timer[ srcId ] <  millis() ) {
+    if ( digit_idx[ srcId ] > 0 || _match[ srcId ] > 0 ) { // reset data --- havn't seen data for a while
+        resetSML( srcId );
+        digitalWrite(  ledPin[ srcId ], HIGH) ; // signal distress - will be reset by usual led-switching 
+        digitalWrite(  ledPin_Board , HIGH) ; // signal distress - will be reset by usual led-switching 
+    } 
+    timer[ srcId  ] = SERIAL_TIMER_DISABLE ;
+  }
 }
 
 void readSML(byte inByte, byte srcId){
   byte match = _match[srcId];
+  timer[ srcId ] = SERIAL_TIMER_CLEAR ;
   // 77 07 01 00   01 08 00   FF 64 1C 21 04 72 62 01 65 03 46 E1 DD 62 1E 52 FF 65   04 35 50 BC   01  --> 1.8.0;70602940 --> 70602940
   // SKIPP:
   // 77 07 01 00   60 32 01   01 01 01 01 01 04 45 46 52 01                                                  --> 96.50.1
   // 77 07 01 00   60 01 00   FF 01 01 01 01 0B 0A 01 45 46 52 20 03 B2 A6 18 01                             --> 96.1.0
   // LOGw(inByte);// use write for bytes ...    rather than Serial.print(inByte);
   if ( pattern_len <= match) {
-    if ( match < row_len[srcId] ) {
+    if ( digit_idx[srcId] >= DIGIT_VALUE_SIZE ) {
+        // digit_idx[srcId]=0 ; // just to be safe !!!
+    } else if ( match < row_len[srcId] ) {
+      if (0 == digit_idx[srcId] /*&& 0 == digit[srcId][digit_idx[srcId]]*/) {
+        digitalWrite( ledPin[srcId] , HIGH);
+      } 
       digit[srcId][digit_idx[srcId]] = digit[srcId][digit_idx[srcId]] * 256 + inByte;
       match++;
     } else {
       // Serial.println(digit[srcId][digit_idx[srcId]]);
+      digitalWrite( ledPin[srcId] , LOW); 
       digit_idx[srcId]++;
-      if (DIGIT_VALUE_SIZE <= digit_idx[srcId]) digit_idx[srcId] = 0; // need  signal for "SML-Message complete"
       match = 0;
     }    
   } else if (inByte == pattern[match])  { // char(FF) != byte(255)  !!!
@@ -110,7 +160,7 @@ void readSML(byte inByte, byte srcId){
   } else if (TYPE == pattern[match] )  { // value tells about values type -- https://www.msxfaq.de/sonst/bastelbude/smartmeter_d0_sml_protokoll.htm
     // 52,53,55,59 => Integer 8/16/32/64          56 => Integer         62,63,65,69 => Unsigned Int 8/16/32/64
     row_len[srcId] = pattern_len + (0x62 == inByte ? 1 : 0x65 == inByte ? 4 : 1 );
-    digit[srcId][digit_idx[srcId]] = 0; // reset .. just in case ... 
+    if (digit_idx[srcId] < DIGIT_VALUE_SIZE) digit[srcId][digit_idx[srcId]] = 0; // reset .. just in case ... 
     match++;
   } else {
     match = 0;
@@ -125,43 +175,60 @@ void readSML(byte inByte, byte srcId){
 
 */
 
+
 void loop() {
 
-  // for tests ...    read from USB-Serial and clone #0 to #1
-  //
-  /*
+
   if (Serial.available() > 0) {
     byte inByte = Serial.read();
     readSML(inByte, 0);
-    readSML(inByte, 1);
-    if (digit_idx[0] >= DIGIT_MAX_VALUE_COUNT) {
-      writeSML();
+    if (digit_idx[0] > DIGIT_MAX_VALUE_COUNT) {
+      Serial.print("H");Serial.print(digit_idx[0]);Serial.print(digit_idx[1]);
     }    
-  }
-  */
+  } else checkTimer( 0) ;
 
-
-
-
-  if (mySerial_0.available() > 0) {
-    readSML(mySerial_0.read(), 0);
-    if (digit_idx[0] >= DIGIT_MAX_VALUE_COUNT) {
-      writeSML();
-      mySerial_1.listen();
-    }
-  }
 
   if (mySerial_1.available() > 0) {
     readSML(mySerial_1.read(), 1);
-    if (digit_idx[1] >= DIGIT_MAX_VALUE_COUNT) {
-      writeSML();
-      mySerial_0.listen();
+    if (digit_idx[1] > DIGIT_MAX_VALUE_COUNT) {
+      Serial.print("S");Serial.print(digit_idx[0]);Serial.print(digit_idx[1]);
     }
+  } else checkTimer( 1) ;
+
+
+  if ((   (0 == digit_idx[0] && digit_idx[1] > 0) || digit_idx[0] >= DIGIT_MAX_VALUE_COUNT) 
+      && ((0 == digit_idx[1] && digit_idx[0] > 0) || digit_idx[1] >= DIGIT_MAX_VALUE_COUNT))  {
+    //serials_reset_time  = SERIAL_RESET_DISABLE; // all good - no need to check ...
+    digitalWrite(ledPin_Board , HIGH);
+    Serial.println("");
+    writeSML();
+    digitalWrite(ledPin_Board , LOW);
+    // resetSML();
   }
+
 
 
 
 /*
+
+1B 1B 1B 1B 01 01 01 01 76 05 00 35 9B C5 62 00 62 00 72 63 01 01 76 01 07 FF FF FF FF FF FF 05 00 11 DE 98 0B 0A 01 45 46 52 20 03 B2 A6 18 72 62 01 65 03 46 E1 DD 01 63 AB 64 00 76 05 00 35 9B C6 62 00 62 00 72 63 07 01 77 07 FF FF FF FF FF FF 0B 0A 01 45 46 52 20 03 B2 A6 18 07 01 00 62 0A FF FF 72 62 01 65 03 46 E1 DD 79 77 07 01 00 60 32 01 01 01 01 01 01 04 45 46 52 01 77 07 01 00 60 01 00 FF 01 01 01 01 0B 0A 01 45 46 52 20 03 B2 A6 18 01 77 07 01 00 01 08 00 FF 64 1C 21 04 72 62 01 65 03 46 E1 DD 62 1E 52 FF 65 04 35 50 BC 01 77 07 01 00 01 08 01 FF 64 1C 21 04 72 62 01 65 03 46 E1 DD 62 1E 52 FF 65 02 BF 84 7A 01 77 07 01 00 01 08 02 FF 64 1C 21 04 72 62 01 65 03 46 E1 DD 62 1E 52 FF 65 01 75 CC 42 01 77 07 01 00 02 08 00 FF 01 72 62 01 65 03 46 E1 DD 62 1E 52 FF 62 10 01 77 07 01 00 02 08 01 FF 01 72 62 01 65 03 46 E1 DD 62 1E 52 FF 62 0E 01 77 07 01 00 02 08 02 FF 01 72 62 01 65 03 46 E1 DD 62 1E 52 FF 62 02 01 77 07 01 00 10 07 00 FF 01 01 62 1B 52 00 52 06 01 01 01 63 5E B8 00 76 05 00 35 9B C7 62 00 62 00 72 63 02 01 71 01 63 90 2D 00 00 1B 1B 1B 1B 1A 01 84 30 
+
+    
+incomplete...
+1B 1B 1B 1B 01 01 01 01 76 05 00 35 9B C5 62 00 62 00 72 63 01 01 76 01 07 FF FF FF FF FF FF 05 00 11 DE 98 0B 0A 01 45 46 52 20 03 B2 A6 18 72 62 01 65 03 46 E1 DD 01 63 AB 64 00 76 05 00 35 9B C6 62 00 62 00 72 63 07 01 77 07 FF FF FF FF FF FF 0B 0A 01 45 46 52 20 03 B2 A6 18 07 01 00 62 0A FF FF 72 62 01 65 03 46 E1 DD 79 77 07 01 00 60 32 01 01 01 01 01 01 04 45 46 52 01 77 07 01 00 60 01 00 FF 01 01 01 01 0B 0A 01 45 46 52 20 03 B2 A6 18 01 77 07 01 00 01 08 00 FF 64 1C 21 04 72 62 01 65 03 46 E1 DD 62 1E 52 FF 65 04 35 50 BC 01 77 07 01 00 01 08 01 FF 64 1C 21 04 72 62 01 65 03 46 E1 DD 62 1E 52 FF 65 02 BF 84 7A 01 77 07 01 00 01 08 02 FF 64 1C 21 04 72 62 01 65 03 46 E1 DD 62 1E 52 FF
+
+
+    = works
+    + add timer to clear incomplete data ... / generic timer
+    - both LED stay on, wont get off
+    + LED per channel, LED on when getting data / off if read
+    ==> SW+HW-Serial can read in parallel !!!!
+    ! make sure waiting HW-AND-SW-SERIAL to feed both with data ...
+    ! write SML erases both -> wait for both to finish ...
+    + just resetSML and do not print ... -> only <HW> Tag recieved, but no <SW>
+    = works - <SW><\r><\n>70602940;46105722;24497218;16;14;2;70602940;46105722;24497218;16;14;2;<\r><\n>   <-- but <HW>-Tag is missing .... and hterm
+    + parallel HW-Serial and SW-Serial
+    - works like sharme .. but may reduce sampling rate 
     + Aktive Switch between both ...
     - erst serial-0.avail(), dann serial-1.avail() => NOTHING   (=|= es ging mal .... warum auch immmer ...)
     - erst serial-1.avail(), dann serial-0.avail() => NOTHING <= wenn beide exakt gleich sind kann nur 1 lesen
