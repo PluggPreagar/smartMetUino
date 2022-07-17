b37efeaa1d6cd8749a4143abc04644b35c7be8cf
pluggPreagar <pluggPreagar@preisfrieden.de>
2022-07-17 22:22:08 +0200

    first pattern for time-extraction

--- smartMetUino.ino
+++ smartMetUino.ino
@@ -1,7 +1,9 @@
 
 #if 0
 
+// may interfere with serial.read ....
 #define LOGw(s) Serial.write(s)
+#define LOGHEX(s) Serial.print(s, HEX)
 #define LOG_(s) Serial.print(s)
 #define LOG(s)  Serial.println(s)
 
@@ -10,6 +12,7 @@
 #define LOGw(s) ;
 #define LOG_(s) ;
 #define LOG(s)  ;
+#define LOGHEX(s);
 
 
 #endif
@@ -34,14 +37,18 @@ byte PLACEHOLDER   = 0xFE;
 byte SKIPP_BETWEEN = 0xFC;
 byte TYPE          = 0xFD;
 //byte pattern[] = {0x77 ,0x07 ,0x01 ,0x00 ,PLACEHOLDER ,0x08 ,PLACEHOLDER ,0xFF ,0x64 ,0x1C ,0x21 ,0x04 ,0x72 ,0x62 ,0x01 ,0x65 ,0x03 ,0x35 ,0xBF ,0x79 ,0x62 ,0x1E ,0x52 ,0xFF ,0x65};
-byte pattern[] = {0x77 ,0x07 ,0x01 ,0x00 ,PLACEHOLDER ,0x08 ,PLACEHOLDER ,0xFF , SKIPP_BETWEEN ,0xFF ,TYPE};
-byte pattern_len = sizeof(pattern)/ sizeof(byte);
+byte _pattern[]         = {0x77 ,0x07 ,0x01 ,0x00 ,PLACEHOLDER ,0x08 ,PLACEHOLDER ,0xFF , SKIPP_BETWEEN ,0xFF ,TYPE};
+byte _pattern_len = sizeof(_pattern)/ sizeof(byte);
+byte _pattern_runtime[] = {0x00 ,0x62 ,0x0A ,0xFF ,0xFF        ,0x72 ,0x62        ,0x01 , TYPE}; // kludge ensure same  length ...
+byte _pattern_runtime_len = sizeof(_pattern_runtime)/ sizeof(byte);
 
-byte row_len[2] = {};
+
+
+byte _row_len[2] = {};
 byte _match[2] = {};
-byte digit_idx[2] = {};
+byte _digit_idx[2] = {};
 
-#define DIGIT_MAX_VALUE_COUNT 6
+#define DIGIT_MAX_VALUE_COUNT 7
 #define DIGIT_VALUE_SIZE DIGIT_MAX_VALUE_COUNT+1
 unsigned long digit[2][ DIGIT_VALUE_SIZE ] = {};
 byte          ledPin[2] = { ledPin_0, ledPin_1  };
@@ -80,10 +87,8 @@ void setup() {
 
   // 
   LOG("ready to mirror your entries!");
-  LOG_("sizeof(pattern) = ");LOG(pattern_len);
-  LOG("wait...");
 
-  //memset(digit_idx, 0, sizeof(digit_idx)); // need to force clear 
+  //memset(_digit_idx, 0, sizeof(_digit_idx)); // need to force clear 
 }
 
 void writeSML(){
@@ -95,19 +100,19 @@ void writeSML(){
   Serial.println("");
   memset(_match, 0, sizeof(_match));
   memset(digit, 0, sizeof(digit));
-  memset(digit_idx, 0, sizeof(digit_idx));
+  memset(_digit_idx, 0, sizeof(_digit_idx));
 }
 
 void resetSML(){
   memset(_match, 0, sizeof(_match));
   memset(digit, 0, sizeof(digit));
-  memset(digit_idx, 0, sizeof(digit_idx));
+  memset(_digit_idx, 0, sizeof(_digit_idx));
 }
 
 void resetSML(byte srcId){
   _match[ srcId ] = 0;
   for(byte i=0; i < DIGIT_MAX_VALUE_COUNT; i++) digit[ srcId ][ i ] = 0;
-  digit_idx[ srcId ] = 0;
+  _digit_idx[ srcId ] = 0;
 }
 
 
@@ -116,7 +121,7 @@ void checkTimer( byte srcId) {
   } else if ( SERIAL_TIMER_CLEAR == timer[ srcId ]) {
     timer[ srcId ] = millis() + SERIAL_TIMER_DELAY ;
   } else if ( timer[ srcId ] <  millis() ) {
-    if ( digit_idx[ srcId ] > 0 || _match[ srcId ] > 0 ) { // reset data --- havn't seen data for a while
+    if ( _digit_idx[ srcId ] > 0 || _match[ srcId ] > 0 ) { // reset data --- havn't seen data for a while
         resetSML( srcId );
         digitalWrite(  ledPin[ srcId ], HIGH) ; // signal distress - will be reset by usual led-switching 
         digitalWrite(  ledPin_Board , HIGH) ; // signal distress - will be reset by usual led-switching 
@@ -125,7 +130,12 @@ void checkTimer( byte srcId) {
   }
 }
 
-void readSML(byte inByte, byte srcId){
+void readSML(byte inByte, byte srcId) {
+  readSML(inByte, srcId,  0 == _digit_idx[srcId] ? _pattern_runtime : _pattern ,  0 == _digit_idx[srcId] ? _pattern_runtime_len : _pattern_len); 
+  //readSML(inByte, srcId,  _pattern ,  _pattern_len); 
+}
+
+void readSML(byte inByte, byte srcId, byte pattern[], byte pattern_len){ 
   byte match = _match[srcId];
   timer[ srcId ] = SERIAL_TIMER_CLEAR ;
   // 77 07 01 00   01 08 00   FF 64 1C 21 04 72 62 01 65 03 46 E1 DD 62 1E 52 FF 65   04 35 50 BC   01  --> 1.8.0;70602940 --> 70602940
@@ -133,45 +143,58 @@ void readSML(byte inByte, byte srcId){
   // 77 07 01 00   60 32 01   01 01 01 01 01 04 45 46 52 01                                                  --> 96.50.1
   // 77 07 01 00   60 01 00   FF 01 01 01 01 0B 0A 01 45 46 52 20 03 B2 A6 18 01                             --> 96.1.0
   // LOGw(inByte);// use write for bytes ...    rather than Serial.print(inByte);
+
+  if (0!=match) LOGHEX(inByte);   
   if ( pattern_len <= match) {
-    if ( digit_idx[srcId] >= DIGIT_VALUE_SIZE ) {
-        // digit_idx[srcId]=0 ; // just to be safe !!!
-    } else if ( match < row_len[srcId] ) {
-      if (0 == digit_idx[srcId] /*&& 0 == digit[srcId][digit_idx[srcId]]*/) {
+    if ( _digit_idx[srcId] >= DIGIT_VALUE_SIZE ) {
+        // _digit_idx[srcId]=0 ; // just to be safe !!!
+    } else if ( match < _row_len[srcId] ) {
+      if (0 == _digit_idx[srcId] /*&& 0 == digit[srcId][_digit_idx[srcId]]*/) {
         digitalWrite( ledPin[srcId] , HIGH);
       } 
-      digit[srcId][digit_idx[srcId]] = digit[srcId][digit_idx[srcId]] * 256 + inByte;
+      digit[srcId][_digit_idx[srcId]] = digit[srcId][_digit_idx[srcId]] * 256 + inByte;
+      LOG_("+");
       match++;
     } else {
-      // Serial.println(digit[srcId][digit_idx[srcId]]);
+      // Serial.println(digit[srcId][_digit_idx[srcId]]);
       digitalWrite( ledPin[srcId] , LOW); 
-      digit_idx[srcId]++;
+      _digit_idx[srcId]++;
+      LOG_("=");LOG_(" ");
       match = 0;
     }    
   } else if (inByte == pattern[match])  { // char(FF) != byte(255)  !!!
+    if( 0 == match) {LOG_("  >");LOGHEX (inByte);};LOG_(".");
     match++;
   } else if (SKIPP_BETWEEN == pattern[match])  { 
     if (inByte == pattern[match+1]) {
       match += 2;
     }
+    LOG_("*");
   } else if (PLACEHOLDER == pattern[match] )  { 
     LOG_(inByte);  LOG_( 0xFF == pattern[match+1] ? ";" : ".");
     match++;
   } else if (TYPE == pattern[match] )  { // value tells about values type -- https://www.msxfaq.de/sonst/bastelbude/smartmeter_d0_sml_protokoll.htm
     // 52,53,55,59 => Integer 8/16/32/64          56 => Integer         62,63,65,69 => Unsigned Int 8/16/32/64
-    row_len[srcId] = pattern_len + (0x62 == inByte ? 1 : 0x65 == inByte ? 4 : 1 );
-    if (digit_idx[srcId] < DIGIT_VALUE_SIZE) digit[srcId][digit_idx[srcId]] = 0; // reset .. just in case ... 
+    _row_len[srcId] = pattern_len + (0x62 == inByte ? 1 : 0x65 == inByte ? 4 : 1 );
+    if (_digit_idx[srcId] < DIGIT_VALUE_SIZE) digit[srcId][_digit_idx[srcId]] = 0; // reset .. just in case ... 
+    LOG_("T");
     match++;
   } else {
+    if (0!=match) {LOG_("<  ");}   
     match = 0;
   } // match
   _match[srcId] = match;
+  if (0!=match) {LOG_(" ");}   
+  
 }
 
 
 /* Test-Data
 
 1B 1B 1B 1B 01 01 01 01 76 05 00 35 9B C5 62 00 62 00 72 63 01 01 76 01 07 FF FF FF FF FF FF 05 00 11 DE 98 0B 0A 01 45 46 52 20 03 B2 A6 18 72 62 01 65 03 46 E1 DD 01 63 AB 64 00 76 05 00 35 9B C6 62 00 62 00 72 63 07 01 77 07 FF FF FF FF FF FF 0B 0A 01 45 46 52 20 03 B2 A6 18 07 01 00 62 0A FF FF 72 62 01 65 03 46 E1 DD 79 77 07 01 00 60 32 01 01 01 01 01 01 04 45 46 52 01 77 07 01 00 60 01 00 FF 01 01 01 01 0B 0A 01 45 46 52 20 03 B2 A6 18 01 77 07 01 00 01 08 00 FF 64 1C 21 04 72 62 01 65 03 46 E1 DD 62 1E 52 FF 65 04 35 50 BC 01 77 07 01 00 01 08 01 FF 64 1C 21 04 72 62 01 65 03 46 E1 DD 62 1E 52 FF 65 02 BF 84 7A 01 77 07 01 00 01 08 02 FF 64 1C 21 04 72 62 01 65 03 46 E1 DD 62 1E 52 FF 65 01 75 CC 42 01 77 07 01 00 02 08 00 FF 01 72 62 01 65 03 46 E1 DD 62 1E 52 FF 62 10 01 77 07 01 00 02 08 01 FF 01 72 62 01 65 03 46 E1 DD 62 1E 52 FF 62 0E 01 77 07 01 00 02 08 02 FF 01 72 62 01 65 03 46 E1 DD 62 1E 52 FF 62 02 01 77 07 01 00 10 07 00 FF 01 01 62 1B 52 00 52 06 01 01 01 63 5E B8 00 76 05 00 35 9B C7 62 00 62 00 72 63 02 01 71 01 63 90 2D 00 00 1B 1B 1B 1B 1A 01 84 30 
+
+-- Time 
+18 07 01 00 62 0A FF FF 72 62 01 65 03 46 E1 DD 79 77 07 01
 
 */
 
@@ -182,23 +205,27 @@ void loop() {
   if (Serial.available() > 0) {
     byte inByte = Serial.read();
     readSML(inByte, 0);
-    if (digit_idx[0] > DIGIT_MAX_VALUE_COUNT) {
-      Serial.print("H");Serial.print(digit_idx[0]);Serial.print(digit_idx[1]);
+    if (_digit_idx[0] > DIGIT_MAX_VALUE_COUNT) {
+      Serial.print("H");Serial.print(_digit_idx[0]);Serial.print(_digit_idx[1]);
     }    
   } else checkTimer( 0) ;
 
 
   if (mySerial_1.available() > 0) {
     readSML(mySerial_1.read(), 1);
-    if (digit_idx[1] > DIGIT_MAX_VALUE_COUNT) {
-      Serial.print("S");Serial.print(digit_idx[0]);Serial.print(digit_idx[1]);
+    if (_digit_idx[1] > DIGIT_MAX_VALUE_COUNT) {
+      Serial.print("S");Serial.print(_digit_idx[0]);Serial.print(_digit_idx[1]);
     }
   } else checkTimer( 1) ;
 
 
-  if ((   (0 == digit_idx[0] && digit_idx[1] > 0) || digit_idx[0] >= DIGIT_MAX_VALUE_COUNT) 
-      && ((0 == digit_idx[1] && digit_idx[0] > 0) || digit_idx[1] >= DIGIT_MAX_VALUE_COUNT))  {
+  if ((   (0 == _digit_idx[0] && _digit_idx[1] > 0) || _digit_idx[0] >= DIGIT_MAX_VALUE_COUNT) 
+      && ((0 == _digit_idx[1] && _digit_idx[0] > 0) || _digit_idx[1] >= DIGIT_MAX_VALUE_COUNT))  {
     //serials_reset_time  = SERIAL_RESET_DISABLE; // all good - no need to check ...
+    // correct times ...
+    digit[0][0] += 1657884311 - 54976989;
+    digit[1][0] += 1657884311 - 54976989;
+    //
     digitalWrite(ledPin_Board , HIGH);
     Serial.println("");
     writeSML();
